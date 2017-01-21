04e34eb3aa506d620767616232bf2b3cfbb519d4
perjo927 <perjo927@student.liu.se>
2017-01-21 18:50:54 +0100

    feat(switch-photos): styling

--- README.md
+++ README.md
@@ -3,9 +3,11 @@ A wall of Photos
 
 ## Getting started
 > git clone **https://github.com/perjo927/preybird-photowall.git**
+
 > cd **preybird-photowall**
 
 ## Install
+_You can also use **[Yarn](https://yarnpkg.com)**_
 > npm install
 
 ## Run
--- preybird-photowall/scripts/app-bundle.js
+++ preybird-photowall/scripts/app-bundle.js
@@ -419,9 +419,201 @@ define('resources/services/flickrPhotosSearchService',["require", "exports", "au
     exports.FlickrPhotosSearchService = FlickrPhotosSearchService;
 });
 
-define('text!app.html', ['module'], function(module) { module.exports = "<template><require from=./styles/main.css></require><require from=./resources/elements/flickr-image.html></require><require from=./resources/elements/search-form.html></require><h1>${title}</h1><search-form search.call=search() text.two-way=searchText>Go</search-form><div class=flex><div class=col repeat.for=\"image of images\"><flickr-image link.bind=image.link linkbig.bind=image.linkBig title.bind=image.title></flickr-image></div></div></template>"; });
+define('resources/configuration/flickrApi.interface',["require", "exports"], function (require, exports) {
+    "use strict";
+});
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t;
+    return { next: verb(0), "throw": verb(1), "return": verb(2) };
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+define('resources/configuration/flickrApiPublic',["require", "exports", "aurelia-framework", "./httpClientConfig", "./../elements/flickr-image"], function (require, exports, aurelia_framework_1, httpClientConfig_1, flickr_image_1) {
+    "use strict";
+    var FlickrApiPublic = (function () {
+        function FlickrApiPublic(httpClientConfig) {
+            this.httpClientConfig = httpClientConfig;
+            this.baseUrl = "https://api.flickr.com/services/feeds/photos_public.gne/?format=json&nojsoncallback=0&tags=";
+            this.httpClient = httpClientConfig.get({
+                baseUrl: this.baseUrl
+            });
+        }
+        FlickrApiPublic.prototype.search = function (text) {
+            return __awaiter(this, void 0, void 0, function () {
+                var response, err_1;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0:
+                            _a.trys.push([0, 2, , 3]);
+                            return [4 /*yield*/, this.httpClient.jsonp(text, 'jsonp')];
+                        case 1:
+                            response = _a.sent();
+                            return [2 /*return*/];
+                        case 2:
+                            err_1 = _a.sent();
+                            console.log(err_1);
+                            return [3 /*break*/, 3];
+                        case 3: return [2 /*return*/];
+                    }
+                });
+            });
+        };
+        FlickrApiPublic.prototype.handle = function (imageData) {
+            var items = imageData.items;
+            var images = [];
+            for (var _i = 0, items_1 = items; _i < items_1.length; _i++) {
+                var i = items_1[_i];
+                images.push(new flickr_image_1.FlickrImage(i.media.m, i.title));
+            }
+            return images;
+        };
+        return FlickrApiPublic;
+    }());
+    FlickrApiPublic = __decorate([
+        aurelia_framework_1.autoinject,
+        __metadata("design:paramtypes", [httpClientConfig_1.HttpClientConfig])
+    ], FlickrApiPublic);
+    exports.FlickrApiPublic = FlickrApiPublic;
+});
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t;
+    return { next: verb(0), "throw": verb(1), "return": verb(2) };
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+define('resources/configuration/flickrApiPhotosSearch',["require", "exports", "aurelia-framework", "./fetchClientConfig"], function (require, exports, aurelia_framework_1, fetchClientConfig_1) {
+    "use strict";
+    var FlickrApiPhotosSearch = (function () {
+        function FlickrApiPhotosSearch(httpClientConfig) {
+            this.httpClientConfig = httpClientConfig;
+            this.baseUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=108af4c065183c8377da15b6eeedd94a&format=json&nojsoncallback=1&text=";
+            this.httpClient = httpClientConfig.get({
+                baseUrl: this.baseUrl
+            });
+        }
+        FlickrApiPhotosSearch.prototype.search = function (text) {
+            return __awaiter(this, void 0, void 0, function () {
+                var fetchOptions, response, data, err_1;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0:
+                            _a.trys.push([0, 3, , 4]);
+                            fetchOptions = {
+                                method: 'GET',
+                                mode: 'cors',
+                                cache: 'default'
+                            };
+                            return [4 /*yield*/, this.httpClient.fetch(text, fetchOptions)];
+                        case 1:
+                            response = _a.sent();
+                            return [4 /*yield*/, response.json()];
+                        case 2:
+                            data = _a.sent();
+                            console.log(data);
+                            return [2 /*return*/];
+                        case 3:
+                            err_1 = _a.sent();
+                            console.log(err_1);
+                            return [3 /*break*/, 4];
+                        case 4: return [2 /*return*/];
+                    }
+                });
+            });
+        };
+        FlickrApiPhotosSearch.prototype.handle = function (imageData) {
+            throw new Error("Not implemented");
+        };
+        return FlickrApiPhotosSearch;
+    }());
+    FlickrApiPhotosSearch = __decorate([
+        aurelia_framework_1.autoinject,
+        __metadata("design:paramtypes", [fetchClientConfig_1.HttpClientConfig])
+    ], FlickrApiPhotosSearch);
+    exports.FlickrApiPhotosSearch = FlickrApiPhotosSearch;
+});
+
+define('text!app.html', ['module'], function(module) { module.exports = "<template><require from=./styles/main.css></require><require from=./resources/elements/flickr-image.html></require><require from=./resources/elements/search-form.html></require><require from=./resources/elements/grid-view.html></require><h1>${title}</h1><search-form search.call=search() text.two-way=searchText>Go</search-form><grid-view><div class=col repeat.for=\"image of images\"><flickr-image link.bind=image.link linkbig.bind=image.linkBig title.bind=image.title></flickr-image></div></grid-view></template>"; });
 define('text!resources/elements/flickr-image.html', ['module'], function(module) { module.exports = "<template bindable=\"link, linkBig, title\"><require from=../../styles/image.css></require><picture><source srcset=${linkBig} media=\"(min-width: 600px)\"><img src=${link} alt=${title}></picture><h5>${title}</h5></template>"; });
-define('text!styles/image.css', ['module'], function(module) { module.exports = "@keyframes fadein {\n  from {\n    opacity: 0; }\n  to {\n    opacity: 1; } }\n\n@keyframes shake {\n  10%, 90% {\n    transform: translate3d(-1px, 0, 0); }\n  20%, 80% {\n    transform: translate3d(2px, 0, 0); }\n  30%, 50%, 70% {\n    transform: translate3d(-4px, 0, 0); }\n  40%, 60% {\n    transform: translate3d(4px, 0, 0); } }\n\nflickr-image {\n  display: block;\n  text-align: center; }\n\npicture {\n  animation: fadein ease-in 1s; }\n\nimg {\n  height: 175px;\n  width: 175px;\n  max-width: 175px;\n  border: 5px solid white;\n  box-shadow: 0px 0px 2px black; }\n\nh5 {\n  margin-top: 0;\n  text-align: center; }\n\n@media screen and (min-width: 1000px) {\n  img {\n    height: 250px;\n    width: 250px;\n    max-width: 250px; } }\n\n@media screen and (max-width: 600px) {\n  img {\n    height: 250px;\n    width: 250px;\n    max-width: 250px; } }\n"; });
+define('text!styles/image.css', ['module'], function(module) { module.exports = "@keyframes fadein {\n  from {\n    opacity: 0; }\n  to {\n    opacity: 1; } }\n\n@keyframes shake {\n  10%, 90% {\n    transform: translate3d(-1px, 0, 0); }\n  20%, 80% {\n    transform: translate3d(2px, 0, 0); }\n  30%, 50%, 70% {\n    transform: translate3d(-4px, 0, 0); }\n  40%, 60% {\n    transform: translate3d(4px, 0, 0); } }\n\nflickr-image {\n  display: block;\n  text-align: center; }\n\npicture {\n  animation: fadein ease-in 1s; }\n\nimg {\n  max-height: 175px;\n  max-width: 175px;\n  border: 5px solid white;\n  box-shadow: 0px 0px 2px black; }\n\nh5 {\n  margin-top: 0;\n  text-align: center; }\n\n@media screen and (min-width: 1000px) {\n  img {\n    max-height: 250px;\n    max-width: 250px; } }\n\n@media screen and (max-width: 600px) {\n  img {\n    max-height: 250px;\n    max-width: 250px; } }\n"; });
 define('text!resources/elements/search-form.html', ['module'], function(module) { module.exports = "<template bindable=\"search, text\"><form submit.trigger=search() class=search><input type=text placeholder=Search value.two-way=text><button type=submit><slot></slot></button></form></template>"; });
 define('text!styles/main.css', ['module'], function(module) { module.exports = "@import url(\"https://fonts.googleapis.com/css?family=Open+Sans|Pacifico\");\n.flex {\n  display: flex;\n  flex-direction: row;\n  flex-flow: wrap;\n  justify-content: space-between;\n  align-items: baseline; }\n  .flex .col {\n    width: 32%;\n    padding: 20px; }\n\n@media screen and (max-width: 600px) {\n  .flex {\n    display: block; }\n    .flex .col {\n      width: 100%;\n      margin: 0 0 10px 0; } }\n\n@keyframes fadein {\n  from {\n    opacity: 0; }\n  to {\n    opacity: 1; } }\n\n@keyframes shake {\n  10%, 90% {\n    transform: translate3d(-1px, 0, 0); }\n  20%, 80% {\n    transform: translate3d(2px, 0, 0); }\n  30%, 50%, 70% {\n    transform: translate3d(-4px, 0, 0); }\n  40%, 60% {\n    transform: translate3d(4px, 0, 0); } }\n\n* {\n  box-sizing: border-box; }\n\nbody {\n  padding: 20px; }\n\nbody, input {\n  font-family: \"Open Sans\", sans-serif;\n  color: darkgray; }\n\nh1 {\n  animation: shake 1s;\n  font-family: \"Pacifico\", cursive;\n  color: mediumvioletred;\n  text-align: center; }\n\nform.search {\n  text-align: center; }\n  form.search input, form.search button {\n    border-radius: 3px; }\n  form.search input {\n    width: 150px;\n    border: 2px solid lightgray; }\n  form.search button {\n    margin-left: 5px;\n    border: 2px solid darkgray;\n    font-family: \"Open Sans\", sans-serif;\n    font-weight: bold;\n    width: 70px;\n    animation: shake 1s;\n    animation-delay: 1s; }\n"; });
+define('text!resources/elements/grid-view.html', ['module'], function(module) { module.exports = "<template><div class=flex><slot></slot></div></template>"; });
 //# sourceMappingURL=app-bundle.js.map
