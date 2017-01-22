caa721a41713b4c3f58170671d721eb550258484
perjo927 <perjo927@student.liu.se>
2017-01-22 15:21:26 +0100

    feat(test-app): add search and app tests

---
+++ preybird-photowall/test/unit/search-form.spec.ts
@@ -0,0 +1,61 @@
+import { bootstrap } from 'aurelia-bootstrapper';
+import { StageComponent } from 'aurelia-testing';
+
+import { SearchForm } from '../../src/resources/elements/search-form';
+
+describe('Given that there is a search form', () => {
+    const searchService = {
+        search: (text: string) => ""
+    };
+    const component = new SearchForm(searchService);
+
+    describe('When the search method is called', () => {
+        beforeEach(() => {
+            spyOn(component.searchService, 'search');
+            component.search("per");
+        });
+        it('Then it should have called the search service', () => {
+            expect(component.searchService.search).toHaveBeenCalled();
+        });
+        it('And it should have called the search service with "per"', () => {
+            expect(component.searchService.search).toHaveBeenCalledWith('per');
+        });
+    });
+});
+
+describe('Given that there is a search component', () => {
+    const searchService = {
+        search: (text: string) => ""
+    };
+    const searchModel = new SearchForm(searchService);
+    const search = () => { searchModel.search('foo') }
+
+    let component;
+
+
+    beforeEach(() => {
+        component = StageComponent
+            .withResources('resources/elements/search-form')
+            .inView('<search-form search.call="search" text.two-way="text"></search-form>')
+            .boundTo({ search: search })
+            .boundTo({ text: 'Per' });
+    });
+
+
+
+    describe('When the component is rendered', () => {
+        it('should have an input with text', done => {
+            component.create(bootstrap).then(() => {
+                const inputElement = document.querySelector('input');
+                const
+                    console.log(inputElement.value, inputElement.search)
+                expect(inputElement.value).toBe('Per');
+                done();
+            }).catch(e => { console.log(e.toString()) });
+        });
+        afterEach(() => {
+            component.dispose();
+        });
+    });
+
+});
